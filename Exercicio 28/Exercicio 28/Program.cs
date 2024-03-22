7fbed656e9adf5cb86d28098cf4b6396283969ab
Leonardo Dallago de Cordova <leodallago@outlook.com>
2024-03-22 09:46:52 -0300

    modificando exercicio 28

--- Exercicio 28/Exercicio 28/Program.cs
+++ Exercicio 28/Exercicio 28/Program.cs
@@ -6,16 +6,47 @@
         {
             Console.WriteLine("Exercicio 28");
 
-          for (int j = 0; j <= 10; j++)
+            Console.WriteLine("Por favor, informe o tipo de taboada (1 para IMPAR) (2 para PAR)");
+            int tipo = Convert.ToInt32(Console.ReadLine());
+
+            int contador = 0;
+
+            if (tipo != 1 && tipo != 2)
+            {
+                Console.WriteLine("Operacao invalida");
+            }
+            else if (tipo == 1)
+            {
+                contador = 1;
+                GeraTaboada(contador);
+
+            }
+
+            else if (tipo == 2)
+            {
+                contador = 0;
+                GeraTaboada(contador);
+
+            }
+
+
+            Console.ReadLine();
+        }
+
+        private static int GeraTaboada(int contador)
+        {
+            while (contador <= 10)
             {
                 for (int i = 0; i <= 10; i++)
                 {
-                    Console.Write($"{i}x{j} = {i * j}");
+                    Console.WriteLine($"{i}x{contador} = {i * contador}");
                 }
                 Console.Write("\n");
+                contador += 2;
+
             }
 
-            Console.ReadLine();
+            return contador;
         }
     }
 }
