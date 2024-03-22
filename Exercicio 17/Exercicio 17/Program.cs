eb6997c50e90779fb90c709a22b041ff22496b67
Leonardo Dallago de Cordova <leodallago@outlook.com>
2024-03-22 17:02:35 -0300

    Refatorando Exercicio 17

--- Exercicio 17/Exercicio 17/Program.cs
+++ Exercicio 17/Exercicio 17/Program.cs
@@ -6,23 +6,19 @@
         {
             Console.WriteLine("Exercicio 17");
 
-            Console.WriteLine("Por favor, informe o PRIMEIRO valor: ");
-            string primeiro = Console.ReadLine();
-            int primeiroValor = Convert.ToInt32(primeiro);
 
-            Console.WriteLine("Por favor, informe o SEGUNDO valor: ");
-            string segundo = Console.ReadLine();
-            int segundoValor = Convert.ToInt32(segundo);
+            int primeiroValor = InformaValor("Por favor, informe o PRIMEIRO valor: ");
 
-            Console.WriteLine("Por favor, informe o TERCEIRO valor");
-            string terceiro = Console.ReadLine();
-            int terceiroValor = Convert.ToInt32(terceiro);
+            int segundoValor = InformaValor("Por favor, informe o SEGUNDO valor: ");
 
-            if(primeiroValor+segundoValor < terceiroValor)
+            int terceiroValor = InformaValor("Por favor, informe o TERCEIRO valor: ");
+
+
+            if (primeiroValor + segundoValor < terceiroValor)
             {
                 Console.WriteLine("A soma do primeiro e segundo valor eh MENOR que o terceiro valor!");
             }
-            else if(primeiroValor + segundoValor == terceiroValor)
+            else if (primeiroValor + segundoValor == terceiroValor)
             {
                 Console.WriteLine("A soma do primeiro e segundo valor eh IGUAL que o terceiro valor!");
             }
@@ -33,6 +29,15 @@
 
             //Mantem o programa aberto (nao apagar)
             Console.ReadLine();
+        }
+
+        static int InformaValor(string valor)
+        {
+            Console.WriteLine(valor);
+
+            int valorPego = Convert.ToInt32(Console.ReadLine());
+
+            return valorPego;
         }
     }
 }
